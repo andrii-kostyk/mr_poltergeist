8a0bbb17df96914aeab81b75237cab25e4c68f35
andrii-kostyk <kostykandri@gmail.com>
2019-10-27 10:31:37 +0000

    daemon proc name

--- app_daemon.rb
+++ app_daemon.rb
@@ -4,6 +4,6 @@ require 'yaml'
 
 GENERAL = YAML.load(File.read(File.join(File.expand_path('..', __FILE__), 'config', "general.yml")))
 
-Daemons.run_proc('mr_poltergeist_app', log_output: false, dir: GENERAL['app_path']) do
+Daemons.run_proc('mr_poltergeist', log_output: false, dir: GENERAL['app_path']) do
   exec "ruby #{GENERAL['app_path']}/app.rb"
 end
